d1a3362207f28552c17414dc3738e69aefe477bb
Henrique Luiz <heenluy@gmail.com>
2024-01-19 13:27:35 -0300

    create the lazy list

--- app/src/main/java/me/henriquelluiz/superheroes/MainActivity.kt
+++ app/src/main/java/me/henriquelluiz/superheroes/MainActivity.kt
@@ -3,13 +3,39 @@ package me.henriquelluiz.superheroes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import me.henriquelluiz.superheroes.model.Hero
+import me.henriquelluiz.superheroes.model.HeroesRepository
 import me.henriquelluiz.superheroes.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +47,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SuperheroApp()
                 }
             }
         }
@@ -29,17 +55,94 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun SuperheroApp() {
+    LazyColumn {
+        items(HeroesRepository.heroes) {
+            SuperheroCard(
+                hero = it,
+                modifier = Modifier
+                    .padding(
+                        bottom = 8.dp,
+                        start = 16.dp,
+                        end = 16.dp
+                    )
+            )
+        }
+    }
+}
+
+@Composable
+fun SuperheroCard(hero: Hero, modifier: Modifier = Modifier) {
+    Card(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 2.dp
+        ),
         modifier = modifier
-    )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium))
+                .sizeIn(minHeight = 72.dp)
+        ) {
+            SuperheroInfo(
+                name = hero.nameRes,
+                description = hero.descriptionRes,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            SuperheroIcon(
+                imageOfHero = hero.imageRes,
+                name = hero.nameRes
+            )
+        }
+    }
+}
+
+@Composable
+fun SuperheroIcon(
+    @DrawableRes imageOfHero: Int,
+    @StringRes name: Int,
+) {
+    Box(
+        modifier = Modifier
+            .size(72.dp)
+            .clip(MaterialTheme.shapes.small)
+    ) {
+        Image(
+            painter = painterResource(imageOfHero),
+            contentDescription = stringResource(name),
+            alignment = Alignment.TopCenter,
+            contentScale = ContentScale.FillWidth
+        )
+    }
+}
+
+@Composable
+fun SuperheroInfo(
+    @StringRes name: Int,
+    @StringRes description: Int,
+    modifier: Modifier = Modifier
+) {
+    Column (modifier = modifier) {
+        Text(
+            text = stringResource(name),
+            style = MaterialTheme.typography.displaySmall
+        )
+
+        Text(
+            text = stringResource(description),
+            style = MaterialTheme.typography.bodyLarge,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SuperheroesTheme {
-        Greeting("Android")
+        SuperheroApp()
     }
 }
